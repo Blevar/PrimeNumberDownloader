877b8ba9210fd261f2618ed24d3e06b69f9c4e31
Jan Prusinowski <jprusinowski@sii.pl>
2023-11-19 11:42:35 +0100

    Added files

---
+++ Tools/FileManagementTools.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static PrimeNumberDownloader.Constants;
+using System.Formats.Tar;
+
+namespace PrimeNumberDownloader
+{
+    internal class FileManagementTools
+    {
+        public static bool CreateFile()
+        {
+            Directory.SetCurrentDirectory(".");
+            if (!File.Exists(DEFAULT_FILE_PATH)) return false;
+            else
+            {
+                File.Create(DEFAULT_FILE_PATH);
+                return true;
+            }
+        }
+    }
+}
---
+++ Tools/WebManagementTools.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PrimeNumberDownloader.Constants;
+using static PrimeNumberDownloader.FileManagementTools;
+
+namespace PrimeNumberDownloader
+{
+    internal class WebManagementTools
+    {
+        static public void CreatePrimesList()
+        {
+            List<string> primesList = new List<string>();
+
+            CreateFile();
+
+            string htmlContent;
+            string url = SOURCE_URL;
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(url);
+            htmlContent = driver.PageSource;
+            StringTools.ExtractPrimesFromPageContent(ref primesList, htmlContent);
+
+            for (int i = 0; i < 10; i++)
+            {
+                IWebElement nextButton = driver.FindElement(By.Name("nextButton"));
+                nextButton.Click();
+                System.Threading.Thread.Sleep(DEFAULT_SLEEP);
+                htmlContent = driver.PageSource;
+                StringTools.ExtractPrimesFromPageContent(ref primesList, htmlContent);
+            }
+        }
+
+        static public async Task<string> DownloadHtmlAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
